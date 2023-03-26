3f8a7de62bbad7fe04e340b5e1f4d04921b5c727
Kavya Sundaresan <80080693+kavash11@users.noreply.github.com>
2023-03-25 21:52:05 -0400

    starter

---
+++ lab5/src/main.c
@@ -0,0 +1,257 @@
+/****** 
+
+1. For GPIO pins, Both OD mode and PP mode can drive the motor! However, some pins cannot output  high in OD mode!!! 
+   
+2. The signals do not need to be inverted before being fed in H-bridge.   
+*/
+
+
+#include "main.h"
+
+
+#define COLUMN(x) ((x) * (((sFONT *)BSP_LCD_GetFont())->Width))    //see font.h, for defining LINE(X)
+
+
+void LCD_DisplayString(uint16_t LineNumber, uint16_t ColumnNumber, uint8_t *ptr);
+void LCD_DisplayInt(uint16_t LineNumber, uint16_t ColumnNumber, int Number);
+void LCD_DisplayFloat(uint16_t LineNumber, uint16_t ColumnNumber, float Number, int DigitAfterDecimalPoint);
+
+
+static void SystemClock_Config(void);
+static void Error_Handler(void);
+
+int main(void){
+	
+		/* STM32F4xx HAL library initialization:
+       - Configure the Flash prefetch, instruction and Data caches
+       - Configure the Systick to generate an interrupt each 1 msec
+       - Set NVIC Group Priority to 4
+       - Global MSP (MCU Support Package) initialization
+     */
+		HAL_Init();
+		
+	
+		 /* Configure the system clock to 72 MHz */
+		SystemClock_Config();
+		
+		HAL_InitTick(0x0000); // set systick's priority to the highest.
+	
+	
+		BSP_LCD_Init();
+		//BSP_LCD_LayerDefaultInit(uint16_t LayerIndex, uint32_t FB_Address);
+		BSP_LCD_LayerDefaultInit(0, LCD_FRAME_BUFFER);   //LCD_FRAME_BUFFER, defined as 0xD0000000 in _discovery_lcd.h
+															// the LayerIndex may be 0 and 1. if is 2, then the LCD is dark.
+		//BSP_LCD_SelectLayer(uint32_t LayerIndex);
+		BSP_LCD_SelectLayer(0);
+		BSP_LCD_SetLayerVisible(0, ENABLE); 
+		
+		BSP_LCD_Clear(LCD_COLOR_WHITE);  //need this line, otherwise, the screen is dark	
+		BSP_LCD_DisplayOn();
+	 
+		BSP_LCD_SetFont(&Font20);  //the default font,  LCD_DEFAULT_FONT, which is defined in _lcd.h, is Font24
+	
+		LCD_DisplayString(2, 3, (uint8_t *)"Lab");
+	
+		LCD_DisplayInt(2, 8, 5);
+			
+	
+		
+		
+		while(1) {	
+
+			
+			
+		} // end of while loop
+	
+}  //end of main
+
+
+/**
+  * @brief  System Clock Configuration
+  *         The system Clock is configured as follow : 
+
+* 					Oscillator											=HSE
+	*    				HSE frequencey 										=8,000,000   (8MHz)
+	*      ----However, if the project is created by uVision, the default HSE_VALUE is 25MHz. thereore, need to define HSE_VALUE
+	*						PLL Source											=HSE
+  *            PLL_M                          = 4
+  *            PLL_N                          = 72
+  *            PLL_P                          = 2
+  *            PLL_Q                          = 3
+  *        --->therefore, PLLCLK =8MHz X N/M/P=72MHz   
+	*            System Clock source            = PLL (HSE)
+  *        --> SYSCLK(Hz)                     = 72,000,000
+  *            AHB Prescaler                  = 1
+	*        --> HCLK(Hz)                       = 72 MHz
+  *            APB1 Prescaler                 = 2
+	*        --> PCLK1=36MHz,  -->since TIM2, TIM3, TIM4 TIM5...are on APB1, thiese TIMs CLK is 36X2=72MHz
+							 	
+  *            APB2 Prescaler                 = 1
+	*        --> PCLK1=72MHz 
+
+  *            VDD(V)                         = 3.3
+  *            Main regulator output voltage  = Scale1 mode
+  *            Flash Latency(WS)              = 5
+  * @param  None
+  * @retval None
+  */
+static void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+
+  /* Enable Power Control clock */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  
+  /* The voltage scaling allows optimizing the power consumption when the device is 
+     clocked below the maximum system frequency, to update the voltage scaling value 
+     regarding system frequency refer to product datasheet.  */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  
+  /* Enable HSE Oscillator and activate PLL with HSE as source */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 4;
+  RCC_OscInitStruct.PLL.PLLN = 72;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 2;
+  if(HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /* Activate the Over-Drive mode */
+  HAL_PWREx_EnableOverDrive();
+ 
+  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 
+     clocks dividers */
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;  
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;  
+  if(HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+
+void LCD_DisplayString(uint16_t LineNumber, uint16_t ColumnNumber, uint8_t *ptr)
+{  
+  //here the LineNumber and the ColumnNumber are NOT  pixel numbers!!!
+		while (*ptr!=NULL)
+    {
+				BSP_LCD_DisplayChar(COLUMN(ColumnNumber),LINE(LineNumber), *ptr); //new version of this function need Xpos first. so COLUMN() is the first para.
+				ColumnNumber++;
+			 //to avoid wrapping on the same line and replacing chars 
+				if ((ColumnNumber+1)*(((sFONT *)BSP_LCD_GetFont())->Width)>=BSP_LCD_GetXSize() ){
+					ColumnNumber=0;
+					LineNumber++;
+				}
+					
+				ptr++;
+		}
+}
+
+void LCD_DisplayInt(uint16_t LineNumber, uint16_t ColumnNumber, int Number)
+{  
+  //here the LineNumber and the ColumnNumber are NOT  pixel numbers!!!
+		char lcd_buffer[15];
+		sprintf(lcd_buffer,"%d",Number);
+	
+		LCD_DisplayString(LineNumber, ColumnNumber, (uint8_t *) lcd_buffer);
+}
+
+void LCD_DisplayFloat(uint16_t LineNumber, uint16_t ColumnNumber, float Number, int DigitAfterDecimalPoint)
+{  
+  //here the LineNumber and the ColumnNumber are NOT  pixel numbers!!!
+		char lcd_buffer[15];
+		
+		sprintf(lcd_buffer,"%.*f",DigitAfterDecimalPoint, Number);  //6 digits after decimal point, this is also the default setting for Keil uVision 4.74 environment.
+	
+		LCD_DisplayString(LineNumber, ColumnNumber, (uint8_t *) lcd_buffer);
+}
+
+
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+	
+		if(GPIO_Pin == KEY_BUTTON_PIN)  //GPIO_PIN_0
+		{
+	
+		}
+		
+		
+		if(GPIO_Pin == GPIO_PIN_1)
+		{
+				
+				
+			
+		}  //end of PIN_1
+
+		if(GPIO_Pin == GPIO_PIN_2) {
+		
+				
+		} //end of if PIN_2	
+		
+		if(GPIO_Pin == GPIO_PIN_3)
+		{
+						
+					
+				
+		} //end of if PIN_3
+}
+
+
+
+void HAL_TIM_OC_DelayElapsedCallback(TIM_HandleTypeDef * htim) //see  stm32fxx_hal_tim.c for different callback function names. 
+{																																//for timer4 
+				//clear the timer counter!  in stm32f4xx_hal_tim.c, the counter is not cleared after  OC interrupt
+				__HAL_TIM_SET_COUNTER(htim, 0x0000);   //this maro is defined in stm32f4xx_hal_tim.h
+
+			
+	
+}
+ 
+static void Error_Handler(void)
+{
+  /* Turn LED4 on */
+  BSP_LED_On(LED4);
+  while(1)
+  {
+  }
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{ 
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+
+

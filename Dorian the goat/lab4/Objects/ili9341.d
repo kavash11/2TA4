183ee9a495f89482756c411f30684964d27adc35
kavash11 <80080693+kavash11@users.noreply.github.com>
2023-05-01 11:12:11 -0400

    deletions

--- Dorian the goat/lab4/Objects/ili9341.d
+++
@@ -1,6 +0,0 @@
-./objects/ili9341.o: \
-  C:\Keil_v5\Keil\STM32F4xx_DFP\2.16.0\Drivers\BSP\Components\ili9341\ili9341.c \
-  RTE\_lab4\Pre_Include_Global.h \
-  C:\Keil_v5\Keil\STM32F4xx_DFP\2.16.0\Drivers\BSP\Components\ili9341\ili9341.h \
-  C:\Keil_v5\Keil\STM32F4xx_DFP\2.16.0\Drivers\BSP\Components\ili9341\..\Common\lcd.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h
